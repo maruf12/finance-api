3cae9a5e58ca65ebda63dd12c2faabecfee2fdb5
maruf12 <maruf.harsono@gmail.com>
2025-08-22 14:21:18 +0700

    First commit

---
+++ prisma/migrations/20250822042051_create_table_user/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "username" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "token" VARCHAR(100),
+    "email" VARCHAR(100),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("username")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
---
+++ src/application/database.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import { logger } from "./logging.js";
+
+export const prismaClient = new PrismaClient({
+  log: [
+    { level: "query", emit: "event" },
+    { level: "error", emit: "event" },
+    { level: "info", emit: "event" },
+    { level: "warn", emit: "event" },
+  ]
+});
+
+prismaClient.$on("error", (e) => {
+  logger.error(e);
+});
+
+prismaClient.$on("warn", (e) => {
+  logger.warn(e);
+});
+
+prismaClient.$on("info", (e) => {
+  logger.info(e);
+});
+
+prismaClient.$on("query", (e) => {
+  logger.query(e);
+});
