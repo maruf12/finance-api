2fa26be1a19900fb67491d5f30110a972245c9a0
maruf12 <maruf.harsono@gmail.com>
2025-08-27 10:53:33 +0700

    Feat/Group: add unit test for update, delete and list

--- test/group.test.js
+++ test/group.test.js
@@ -85,3 +85,131 @@ describe('GET /api/groups/:groupId', () => {
     expect(result.body.errors).toBeDefined();
   });
 })
+
+describe('PUT /api/groups/:groupId', () => {
+  beforeEach(async () => {
+    await createTestUser();
+    await createTestGroup();
+  });
+
+  afterEach(async () => {
+    await removeAllTestGroup();
+    await removeTestUser();
+  });
+
+  it('should update a group by id', async () => {
+    const testGroup = await getTestGroup();
+    const result = await supertest(web)
+      .put(`/api/groups/${testGroup.id}`)
+      .set('Authorization', `test`)
+      .send({
+        name: 'Updated Test Group',
+        description: 'This is an updated test group',
+      });
+
+      logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.name).toBe('Updated Test Group');
+    expect(result.body.data.description).toBe('This is an updated test group');
+    expect(result.body.data.id).toBeDefined();
+    expect(result.body.data.createdAt).toBeDefined();
+    expect(result.body.data.updatedAt).toBeDefined();
+  });
+
+  it('should reject if request invalid', async () => {
+    const testGroup = await getTestGroup();
+    const result = await supertest(web)
+      .put(`/api/groups/${testGroup.id}`)
+      .set('Authorization', `test`)
+      .send({
+        name: '',
+        description: '',
+      });
+    
+      logger.info(result.body);
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it('should reject if group not found', async () => {
+    const testGroup = await getTestGroup();
+    const result = await supertest(web)
+      .put(`/api/groups/${testGroup.id}i`)
+      .set('Authorization', `test`)
+      .send({
+        name: 'Updated Test Group',
+        description: 'This is an updated test group',
+      });
+    
+      logger.info(result.body);
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+})
+
+describe('DELETE /api/groups/:groupId', () => {
+  beforeEach(async () => {
+    await createTestUser();
+    await createTestGroup();
+  });
+
+  afterEach(async () => {
+    await removeAllTestGroup();
+    await removeTestUser();
+  });
+
+  it('should delete a group by id', async () => {
+    const testGroup = await getTestGroup();
+    const result = await supertest(web)
+      .delete(`/api/groups/${testGroup.id}`)
+      .set('Authorization', `test`);
+
+      // logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data).toBe('OK');
+  });
+
+  it('should reject if group not found', async () => {
+    const testGroup = await getTestGroup();
+    const result = await supertest(web)
+      .delete(`/api/groups/${testGroup.id}i`)
+      .set('Authorization', `test`);
+    
+      // logger.info(result.body);
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+})
+
+describe('GET /api/groups', () => {
+  beforeEach(async () => {
+    await createTestUser();
+    await createTestGroup();
+  });
+
+  afterEach(async () => {
+    await removeAllTestGroup();
+    await removeTestUser();
+  });
+
+  it('should get all groups', async () => {
+    const result = await supertest(web)
+      .get('/api/groups')
+      .set('Authorization', `test`);
+
+      // logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.length).toBe(1);
+    expect(result.body.data[0].name).toBe('Test Group');
+    expect(result.body.data[0].description).toBe('This is a test group');
+    expect(result.body.data[0].id).toBeDefined();
+    expect(result.body.data[0].createdAt).toBeDefined();
+    expect(result.body.data[0].updatedAt).toBeDefined();
+  });
+})
